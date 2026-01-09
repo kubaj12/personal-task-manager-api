1ced1d825b8290246ed5d73b89b139e6d742807e
Jakub Jakimiuk <jakubjakimiuk12@gmail.com>
2026-01-09 16:14:18 +0100

    Initial commit - project configuration

---
+++ src/main/java/io/github/kubaj12/personal_task_manager_api/PersonalTaskManagerApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.kubaj12.personal_task_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalTaskManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalTaskManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/kubaj12/personal_task_manager_api/PersonalTaskManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kubaj12.personal_task_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalTaskManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
