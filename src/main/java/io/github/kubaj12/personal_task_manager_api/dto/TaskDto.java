3c05900cb5a90af709e747c22c9363363d958b55
Jakub Jakimiuk <jakubjakimiuk12@gmail.com>
2026-01-15 12:49:07 +0100

    refactor: add @Builder to record TaskDto

--- src/main/java/io/github/kubaj12/personal_task_manager_api/dto/TaskDto.java
+++ src/main/java/io/github/kubaj12/personal_task_manager_api/dto/TaskDto.java
@@ -2,8 +2,10 @@ package io.github.kubaj12.personal_task_manager_api.dto;
 
 import io.github.kubaj12.personal_task_manager_api.entity.TaskPriority;
 import io.github.kubaj12.personal_task_manager_api.entity.TaskStatus;
+import lombok.Builder;
 
 import java.time.OffsetDateTime;
 
+@Builder
 record TaskDto(Long id, String title, String description, TaskStatus status, TaskPriority priority, OffsetDateTime deadline) {
 }
