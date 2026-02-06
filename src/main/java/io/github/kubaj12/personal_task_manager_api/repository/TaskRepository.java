e7c340a516b9660c99b3f334b84b2a495e1d320a
Jakub Jakimiuk <jakubjakimiuk12@gmail.com>
2026-02-06 20:29:42 +0100

    refactor(TaskRepository): change TaskRepository to JpaRepository from CrudRepository

--- src/main/java/io/github/kubaj12/personal_task_manager_api/repository/TaskRepository.java
+++ src/main/java/io/github/kubaj12/personal_task_manager_api/repository/TaskRepository.java
@@ -1,7 +1,8 @@
 package io.github.kubaj12.personal_task_manager_api.repository;
 
 import io.github.kubaj12.personal_task_manager_api.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface TaskRepository extends CrudRepository<Task, Long>, PagingAndSortingRepository<Task, Long> {}
+public interface TaskRepository extends JpaRepository<Task, Long> {}
--- src/test/java/io/github/kubaj12/personal_task_manager_api/PersonalTaskManagerApiApplicationTests.java
+++ src/test/java/io/github/kubaj12/personal_task_manager_api/PersonalTaskManagerApiApplicationTests.java
@@ -3,11 +3,18 @@ package io.github.kubaj12.personal_task_manager_api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class PersonalTaskManagerApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void test() {
+		assertThat(1).isEqualTo(1);
 	}
 
 }
