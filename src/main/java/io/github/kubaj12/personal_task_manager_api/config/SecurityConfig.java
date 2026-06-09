bda86cf94299e54941aaf7bd604dedfea1fd74df
Jakub Jakimiuk <jakubjakimiuk12@gmail.com>
2026-06-09 20:26:52 +0200

    feat: add jwtEncoder bean to security config

--- src/main/java/io/github/kubaj12/personal_task_manager_api/config/SecurityConfig.java
+++ src/main/java/io/github/kubaj12/personal_task_manager_api/config/SecurityConfig.java
@@ -1,5 +1,11 @@
 package io.github.kubaj12.personal_task_manager_api.config;
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +15,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -43,5 +51,12 @@ public class SecurityConfig {
   @Bean
   JwtDecoder jwtDecoder() {
     return NimbusJwtDecoder.withPublicKey(rsaKeys.publicKey()).build();
+  }
+
+  @Bean
+  JwtEncoder jwtEncoder() {
+    JWK jwk = new RSAKey.Builder(rsaKeys.publicKey()).privateKey(rsaKeys.privateKey()).build();
+    JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+    return new NimbusJwtEncoder(jwks);
   }
 }
